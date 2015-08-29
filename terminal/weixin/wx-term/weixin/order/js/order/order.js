68ea15bc31931fbba1e129ffd51ad059d8fb49f1
skipper0110 <745112388@qq.com>
2015-08-29 18:15:31 +0800

    预定

--- terminal/weixin/wx-term/weixin/order/js/order/order.js
+++ terminal/weixin/wx-term/weixin/order/js/order/order.js
@@ -25,7 +25,23 @@ var Templet = {
 			'<div {display}>菜名: <span style="font-weight: bold;">{foods}</span></div>' +
 			'<div>电话: <span style="font-weight: bold;">{phone}</span></div>' +
 			'<div>地址: <span style="font-weight: bold;">{address}</span></div>' +			
-		'</div>'			
+		'</div>',
+	book : '<div class="box-order orderBorder_commit">'	+
+				'<div class="box-order-commit"><div style="height: 20px;">' +
+					'<div style="float: left;">订单号:{code}</div>' +
+					'<div style="float: right;color:#26a9d0;">{status}</div>' +
+				'</div></div>' +
+				'<div>预定日期: <span style="font-weight: bold">{orderDate}</span></div>' +
+				'<div>' +
+				'<div style="float: left">定金: <span style="font-weight: bold;color: red;">{totalPrice}</span> 元</div>' +
+				'<div style="float: right">预定菜品: <span style="font-weight: bold;color: red;">{count}</span> 项</div>' +
+				'</div>' +
+				'<div {display}>菜名: <span style="font-weight: bold;">{foods}</span></div>' +
+				'<div style="margin-top: 5px;height: 33px;width: 99%;text-align: center;" >' +
+					'<a class="a_demo_two" style="font-size: 20px;font-weight: bold;" onclick="buildInvitation({bookId})">发送邀请函</a>' +		
+				'</div>	'+	
+			'</div>'
+	
 };
 
 function changWeixinOrderList(otype){
@@ -45,6 +61,7 @@ function buildInvitation(bookId){
 
 $(function(){
 	Util.lbar('', function(html){ $(document.body).append(html);  });
+	//获取订单列表
 	$.ajax({
 		url : '../../WXQueryOrder.do',
 		dataType : 'json',
@@ -96,6 +113,50 @@ $(function(){
 
 			}
 			$('#divWeixinOrderList').html(html.join(''));
+		},
+		error : function(xhr, errorType, error){}
+	});
+	
+	//获取预定列表
+	$.ajax({
+		url : '../../WXQueryBook.do',
+		dataType : 'json',
+		data : {
+			dataSource : 'bookList',
+			oid : Util.mp.oid,
+			fid : Util.mp.fid
+		},
+		success : function(data, status, xhr){
+			var html = [];
+			for(var i = 0; i < data.root.length; i++){
+				var temp = data.root[i];
+				var totalPrice = 0, count = 0, foods = '';
+				
+				if(temp.order){
+					count = temp.order.orderFoods.length;
+					for (var j = 0; j < temp.order.orderFoods.length; j++) {
+						totalPrice += temp.order.orderFoods[j].totalPrice;
+						if(foods){
+							foods += '，';
+						}
+						foods += temp.order.orderFoods[j].foodName;
+					}				
+				}
+
+				
+				html.push(Templet.book.format({
+					code : temp.id,
+					status : temp.statusDesc,
+					orderDate : temp.bookDate,
+					totalPrice : temp.money,
+					count : count,
+					display : !foods ? 'hidden="hidden"' : '',
+					foods : foods,
+					bookId : temp.id
+				}));						
+
+			}
+			$('#divWeixinBookList').html(html.join(''));
 		},
 		error : function(xhr, errorType, error){}
 	});
