cde5f497afc294fcd5f7930fbbb999cd92864ffd
yzhang1981 <21226674@qq.com>
2015-10-23 18:02:12 +0800

    add the chart for member price by each day

--- db-adaptor/src/com/wireless/db/billStatistics/CalcMemberPriceDao.java
+++ db-adaptor/src/com/wireless/db/billStatistics/CalcMemberPriceDao.java
@@ -1,7 +1,11 @@
 package com.wireless.db.billStatistics;
 
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import com.wireless.db.DBCon;
@@ -10,10 +14,12 @@ import com.wireless.db.Params;
 import com.wireless.exception.BusinessException;
 import com.wireless.pojo.billStatistics.DutyRange;
 import com.wireless.pojo.billStatistics.HourRange;
+import com.wireless.pojo.billStatistics.member.MemberPriceByEachDay;
 import com.wireless.pojo.dishesOrder.Order;
 import com.wireless.pojo.regionMgr.Table;
 import com.wireless.pojo.staffMgr.Staff;
 import com.wireless.pojo.util.DateType;
+import com.wireless.pojo.util.DateUtil;
 
 public class CalcMemberPriceDao {
 	
@@ -108,6 +114,84 @@ public class CalcMemberPriceDao {
 		
 		return result;
 		
+	}
+
+	/**
+	 * Calculate the member price by each day.
+	 * @param staff
+	 * 			the staff to perform this action
+	 * @param dutyRange
+	 * 			the duty range
+	 * @param extraCond
+	 * 			the extra condition
+	 * @return the result list
+	 * @throws SQLException
+	 * 			throws if failed to execute any SQL statement
+	 * @throws ParseException
+	 */
+	public static List<MemberPriceByEachDay> calcMemberPriceByEachDay(Staff staff, DutyRange dutyRange, ExtraCond extraCond) throws SQLException, ParseException{
+		DBCon dbCon = new DBCon();
+		try{
+			dbCon.connect();
+			return calcMemberPriceByEachDay(dbCon, staff, dutyRange, extraCond);
+		}finally{
+			dbCon.disconnect();
+		}
+	}
+	
+	/**
+	 * Calculate the member price by each day.
+	 * @param dbCon
+	 * 			the database connection
+	 * @param staff
+	 * 			the staff to perform this action
+	 * @param dutyRange
+	 * 			the duty range
+	 * @param extraCond
+	 * 			the extra condition
+	 * @return the result list
+	 * @throws SQLException
+	 * 			throws if failed to execute any SQL statement
+	 * @throws ParseException
+	 */
+	public static List<MemberPriceByEachDay> calcMemberPriceByEachDay(DBCon dbCon, Staff staff, DutyRange dutyRange, ExtraCond extraCond) throws SQLException, ParseException{
+		List<MemberPriceByEachDay> result = new ArrayList<MemberPriceByEachDay>();
+		
+		Calendar c = Calendar.getInstance();
+		Date dateBegin = new SimpleDateFormat("yyyy-MM-dd").parse(dutyRange.getOnDutyFormat());
+		Date dateEnd = new SimpleDateFormat("yyyy-MM-dd").parse(dutyRange.getOffDutyFormat());
+		c.setTime(dateBegin);
+		while (dateBegin.compareTo(dateEnd) <= 0) {
+			c.add(Calendar.DATE, 1);
+			
+			DutyRange range = DutyRangeDao.exec(dbCon, staff, 
+					DateUtil.format(dateBegin, DateUtil.Pattern.DATE_TIME), 
+					DateUtil.format(c.getTime(), DateUtil.Pattern.DATE_TIME));
+			
+			if(range != null){
+				String sql;
+				sql = " SELECT " +
+					  " COUNT(*) AS amount, " +
+					  " ROUND(SUM(TMP.actual_price), 2) AS price " +
+				      " FROM (" +
+				      makeSql4MemberPrice(staff, range, extraCond) + 
+					  " ) AS TMP ";
+			    dbCon.rs = dbCon.stmt.executeQuery(sql);
+				if(dbCon.rs.next()){
+					result.add(new MemberPriceByEachDay(new DutyRange(dateBegin.getTime(), c.getTimeInMillis()),
+														 dbCon.rs.getFloat("amount"),
+														 dbCon.rs.getFloat("price")));
+				}
+				dbCon.rs.close();
+
+			}else{
+				result.add(new MemberPriceByEachDay(new DutyRange(dateBegin.getTime(), c.getTimeInMillis()), 0, 0)); 
+			}
+			
+			dateBegin = c.getTime();
+		}
+		
+		return result;
 	}
 	
 	private static String makeSql4MemberPrice(Staff staff, DutyRange range, ExtraCond extraCond){
