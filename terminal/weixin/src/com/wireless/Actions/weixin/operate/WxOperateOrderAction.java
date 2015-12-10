2c0a6ebef0b1677c43809ed53b6150a5f5e91721
yzhang1981 <21226674@qq.com>
2015-12-10 12:19:33 +0800

    add the food unit to weixin order

--- terminal/weixin/src/com/wireless/Actions/weixin/operate/WxOperateOrderAction.java
+++ terminal/weixin/src/com/wireless/Actions/weixin/operate/WxOperateOrderAction.java
@@ -11,6 +11,7 @@ import org.apache.struts.actions.DispatchAction;
 import com.wireless.db.member.MemberDao;
 import com.wireless.db.member.TakeoutAddressDao;
 import com.wireless.db.menuMgr.FoodDao;
+import com.wireless.db.menuMgr.FoodUnitDao;
 import com.wireless.db.regionMgr.TableDao;
 import com.wireless.db.staffMgr.StaffDao;
 import com.wireless.db.weixin.order.WxOrderDao;
@@ -18,11 +19,15 @@ import com.wireless.db.weixin.restaurant.WxRestaurantDao;
 import com.wireless.exception.BusinessException;
 import com.wireless.exception.TableError;
 import com.wireless.json.JObject;
+import com.wireless.pack.ProtocolPackage;
+import com.wireless.pack.Type;
+import com.wireless.pack.req.ReqPrintContent;
 import com.wireless.pojo.dishesOrder.OrderFood;
 import com.wireless.pojo.member.Member;
 import com.wireless.pojo.member.TakeoutAddress;
 import com.wireless.pojo.staffMgr.Staff;
 import com.wireless.pojo.weixin.order.WxOrder;
+import com.wireless.sccon.ServerConnector;
 
 public class WxOperateOrderAction extends DispatchAction {
 	
@@ -77,15 +82,24 @@ public class WxOperateOrderAction extends DispatchAction {
 					String orderFoods[] = of.split(",");
 					OrderFood orderFood = new OrderFood(FoodDao.getById(staff, Integer.parseInt(orderFoods[0])));
 					orderFood.setCount(Float.parseFloat(orderFoods[1]));
+					if(orderFoods.length > 2){
+						orderFood.setFoodUnit(FoodUnitDao.getById(staff, Integer.parseInt(orderFoods[2])));
+					}
 					
 					builder.add(orderFood);
 				}
 			}
 			
 			final int wxOrderId = WxOrderDao.insert(staff, builder);
-			
 			jObject.setExtra(WxOrderDao.getById(staff, wxOrderId));
-			jObject.initTip(true, "下单成功");
+			
+			//打印微信账单
+			ProtocolPackage resp = ServerConnector.instance().ask(ReqPrintContent.buildWxOrder(staff, wxOrderId).build());
+			if(resp.header.type == Type.ACK){
+				jObject.initTip(true, "下单成功(完成打印)");
+			}else{
+				jObject.initTip(true, "下单成功");
+			}
 			
 		}catch(BusinessException e){
 			e.printStackTrace();
