54a40f1ac96ef67ac181a542e7c4928e1a632dba
yzhang1981 <21226674@qq.com>
2015-12-17 18:47:32 +0800

    add the weixin msg in case of charging

--- terminal/weixin/src/com/wireless/Actions/weixin/query/WxNotifyMemberAction.java
+++ terminal/weixin/src/com/wireless/Actions/weixin/query/WxNotifyMemberAction.java
@@ -23,10 +23,40 @@ import com.wireless.pojo.member.MemberOperation;
 import com.wireless.pojo.member.WxMember;
 import com.wireless.pojo.staffMgr.Staff;
 import com.wireless.pojo.util.DateType;
+import com.wireless.pojo.util.DateUtil;
 import com.wireless.pojo.util.NumericUtil;
 import com.wireless.pojo.weixin.restaurant.WxRestaurant;
 
 public class WxNotifyMemberAction extends DispatchAction{
+	
+	//发送充值模板信息
+	public ActionForward charge(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		final String moId = request.getParameter("moId");
+		final String staffId = request.getParameter("staffId");
+		final Staff staff = StaffDao.getById(Integer.parseInt(staffId));
+		final MemberOperation mo = MemberOperationDao.getById(staff, DateType.TODAY, Integer.parseInt(moId));
+		final WxRestaurant wxRestaurant = WxRestaurantDao.get(staff);
+		if(wxRestaurant.hasChargeTemplate() && wxRestaurant.hasQrCode()){
+			final WxMember wxMember = WxMemberDao.getByMember(staff, mo.getMemberId());
+			//String appId = "wx49b3278a8728ff76";
+			//String appSecret = "0ba130d87e14a1a37e20c78a2b0ee3ba";
+			//final Token token = Token.newInstance(appId, appSecret);
+			final AuthorizerToken authorizerToken = AuthorizerToken.newInstance(AuthParam.COMPONENT_ACCESS_TOKEN, wxRestaurant.getWeixinAppId(), wxRestaurant.getRefreshToken());
+			final Token token = Token.newInstance(authorizerToken);
+			Template.send(token, new Template.Builder()
+					.setToUser(wxMember.getSerial())
+					.setTemplateId(wxRestaurant.getChargeTemplate())
+					.addKeyword(new Keyword("first", "亲爱的会员，充值已成功"))
+					.addKeyword(new Keyword("keyword1", wxRestaurant.getNickName()))		//餐厅名称
+					.addKeyword(new Keyword("keyword2", DateUtil.format(mo.getOperateDate())))	//充值时间
+					.addKeyword(new Keyword("keyword3", NumericUtil.CURRENCY_SIGN + NumericUtil.float2String2(mo.getChargeMoney())))	//充值金额
+					.addKeyword(new Keyword("keyword4", NumericUtil.CURRENCY_SIGN + NumericUtil.float2String2(mo.getDeltaBaseMoney() + mo.getDeltaExtraMoney() - mo.getChargeMoney())))				//赠送金额
+					.addKeyword(new Keyword("keyword5", NumericUtil.CURRENCY_SIGN + NumericUtil.float2String2(mo.getRemainingTotalMoney())))  //可用余额
+					.addKeyword(new Keyword("remark", "如有疑问，请联系商家客服。"))
+					);
+		}	
+		return null;
+	}
 	
 	//发送账单模板信息
 	public ActionForward bill(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -40,9 +70,6 @@ public class WxNotifyMemberAction extends DispatchAction{
 		final WxRestaurant wxRestaurant = WxRestaurantDao.get(staff);
 		if(wxRestaurant.hasPaymentTemplate() && wxRestaurant.hasQrCode()){
 			final WxMember wxMember = WxMemberDao.getByMember(staff, order.getMemberId());
-//			System.out.println("serial:" + wxMember.getSerial());
-//			System.out.println("weixin app id:" + wxRestaurant.getWeixinAppId());
-//			System.out.println("weixin app secret:" + wxRestaurant.getWeixinAppSecret());
 //			String appId = "wx49b3278a8728ff76";
 //			String appSecret = "0ba130d87e14a1a37e20c78a2b0ee3ba";
 //			final Token token = Token.newInstance(appId, appSecret);
