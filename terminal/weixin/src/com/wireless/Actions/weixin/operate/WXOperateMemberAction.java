f841e20a693826f66d8a50a7429285402930087c
yzhang1981 <21226674@qq.com>
2015-12-18 15:08:25 +0800

    fix the restaurant expired weixin notification

--- terminal/weixin/src/com/wireless/Actions/weixin/operate/WXOperateMemberAction.java
+++ terminal/weixin/src/com/wireless/Actions/weixin/operate/WXOperateMemberAction.java
@@ -1,6 +1,5 @@
 package com.wireless.Actions.weixin.operate;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -20,7 +19,6 @@ import com.wireless.db.orderMgr.OrderDao;
 import com.wireless.db.orderMgr.PayOrder;
 import com.wireless.db.promotion.CouponDao;
 import com.wireless.db.restaurantMgr.RestaurantDao;
-import com.wireless.db.sms.VerifySMSDao;
 import com.wireless.db.staffMgr.StaffDao;
 import com.wireless.db.weixin.member.WxMemberDao;
 import com.wireless.db.weixin.restaurant.WxRestaurantDao;
@@ -33,12 +31,7 @@ import com.wireless.pojo.member.Member;
 import com.wireless.pojo.member.WxMember;
 import com.wireless.pojo.promotion.Coupon;
 import com.wireless.pojo.restaurantMgr.Restaurant;
-import com.wireless.pojo.sms.VerifySMS;
-import com.wireless.pojo.sms.VerifySMS.ExpiredPeriod;
-import com.wireless.pojo.sms.VerifySMS.InsertBuilder;
 import com.wireless.pojo.staffMgr.Staff;
-import com.wireless.sms.SMS;
-import com.wireless.sms.msg.Msg4Verify;
 
 public class WXOperateMemberAction extends DispatchAction {
 	
@@ -57,12 +50,12 @@ public class WXOperateMemberAction extends DispatchAction {
 		JObject jobject = new JObject();
 		DBCon dbCon = new DBCon();
 		String openId = request.getParameter("oid");
-		String formId = request.getParameter("fid");
+		String fromId = request.getParameter("fid");
 		int rid = 0;
 		try{
 
 			dbCon.connect();
-			rid = WxRestaurantDao.getRestaurantIdByWeixin(dbCon, formId);
+			rid = WxRestaurantDao.getRestaurantIdByWeixin(dbCon, fromId);
 			final Staff staff = StaffDao.getAdminByRestaurant(rid);
 			final Restaurant restaurant = RestaurantDao.getById(dbCon, rid);
 			
@@ -117,10 +110,7 @@ public class WXOperateMemberAction extends DispatchAction {
 				
 			});
 			
-		}catch(BusinessException e){
-			e.printStackTrace();
-			jobject.initTip(e);
-		}catch(SQLException e){
+		}catch(BusinessException | SQLException e){
 			e.printStackTrace();
 			jobject.initTip(e);
 		}catch(Exception e){
@@ -142,66 +132,61 @@ public class WXOperateMemberAction extends DispatchAction {
 	 * @return
 	 * @throws Exception
 	 */
-	public ActionForward getVerifyCode(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		request.setCharacterEncoding("UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		JObject jobject = new JObject();
-		DBCon dbCon = new DBCon();
-		try{
-			dbCon.connect();
-			dbCon.conn.setAutoCommit(false);
-			
-			String mobile = request.getParameter("mobile");
-			String fromId = request.getParameter("fid");
-
-			final VerifySMS sms = VerifySMSDao.getById(dbCon, VerifySMSDao.insert(dbCon, new InsertBuilder(ExpiredPeriod.MINUTE_10)));
-			int restaurantId = WxRestaurantDao.getRestaurantIdByWeixin(dbCon, fromId);
-			SMS.send(dbCon, StaffDao.getAdminByRestaurant(restaurantId), mobile, new Msg4Verify(sms.getCode()));
-			dbCon.conn.commit();
-			
-			jobject.initTip(true, "操作成功, 已发送短信验证码, 请注意查看.");
-			jobject.setExtra(new Jsonable(){
-
-				@Override
-				public JsonMap toJsonMap(int flag) {
-					JsonMap jm = new JsonMap();
-					jm.putJsonable("code", sms, 0);
-					return jm;
-				}
-
-				@Override
-				public void fromJsonMap(JsonMap jsonMap, int flag) {
-					
-				}
-				
-			});
-			
-		}catch(BusinessException e ){
-			dbCon.conn.rollback();
-			e.printStackTrace();
-			jobject.initTip(e);
-			
-		}catch(SQLException e){
-			dbCon.conn.rollback();
-			e.printStackTrace();
-			jobject.initTip(e);
-			
-		}catch(IOException e){
-			dbCon.conn.rollback();
-			e.printStackTrace();
-			jobject.initTip4Exception(e);
-			
-		}catch(Exception e){
-			dbCon.conn.rollback();
-			e.printStackTrace();
-			jobject.initTip4Exception(e);
-			
-		}finally{
-			dbCon.disconnect();
-			response.getWriter().print(jobject.toString());
-		}
-		return null;
-	}
+//	public ActionForward getVerifyCode(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		request.setCharacterEncoding("UTF-8");
+//		response.setCharacterEncoding("UTF-8");
+//		JObject jobject = new JObject();
+//		DBCon dbCon = new DBCon();
+//		try{
+//			dbCon.connect();
+//			dbCon.conn.setAutoCommit(false);
+//			
+//			String mobile = request.getParameter("mobile");
+//			String fromId = request.getParameter("fid");
+//
+//			final VerifySMS sms = VerifySMSDao.getById(dbCon, VerifySMSDao.insert(dbCon, new InsertBuilder(ExpiredPeriod.MINUTE_10)));
+//			int restaurantId = WxRestaurantDao.getRestaurantIdByWeixin(dbCon, fromId);
+//			SMS.send(dbCon, StaffDao.getAdminByRestaurant(restaurantId), mobile, new Msg4Verify(sms.getCode()));
+//			dbCon.conn.commit();
+//			
+//			jobject.initTip(true, "操作成功, 已发送短信验证码, 请注意查看.");
+//			jobject.setExtra(new Jsonable(){
+//
+//				@Override
+//				public JsonMap toJsonMap(int flag) {
+//					JsonMap jm = new JsonMap();
+//					jm.putJsonable("code", sms, 0);
+//					return jm;
+//				}
+//
+//				@Override
+//				public void fromJsonMap(JsonMap jsonMap, int flag) {
+//					
+//				}
+//				
+//			});
+//			
+//		}catch(BusinessException  | SQLException e){
+//			dbCon.conn.rollback();
+//			e.printStackTrace();
+//			jobject.initTip(e);
+//			
+//		}catch(IOException e){
+//			dbCon.conn.rollback();
+//			e.printStackTrace();
+//			jobject.initTip4Exception(e);
+//			
+//		}catch(Exception e){
+//			dbCon.conn.rollback();
+//			e.printStackTrace();
+//			jobject.initTip4Exception(e);
+//			
+//		}finally{
+//			dbCon.disconnect();
+//			response.getWriter().print(jobject.toString());
+//		}
+//		return null;
+//	}
 	
 	/**
 	 * 
@@ -313,13 +298,10 @@ public class WXOperateMemberAction extends DispatchAction {
 			});
 			
 			dbCon.conn.commit();
-		}catch(BusinessException e){
+		}catch(BusinessException | SQLException e){
 			e.printStackTrace();
 			jobject.initTip(e);
 			
-		}catch(SQLException e){
-			e.printStackTrace();
-			jobject.initTip(e);
 		}finally{
 			dbCon.disconnect();
 			response.getWriter().print(jobject.toString());
@@ -371,13 +353,10 @@ public class WXOperateMemberAction extends DispatchAction {
 			});
 			
 			dbCon.conn.commit();
-		}catch(BusinessException e){
+		}catch(BusinessException | SQLException e){
 			e.printStackTrace();
 			jobject.initTip(e);
 			
-		}catch(SQLException e){
-			e.printStackTrace();
-			jobject.initTip(e);
 		}finally{
 			dbCon.disconnect();
 			response.getWriter().print(jobject.toString());
@@ -385,99 +364,5 @@ public class WXOperateMemberAction extends DispatchAction {
 		return null;
 	}
 	
-	/**
-	 * 重新绑定手机号码
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-	public ActionForward rebind(ActionMapping mapping, ActionForm form,	HttpServletRequest request, HttpServletResponse response) throws Exception {
-//		request.setCharacterEncoding("UTF-8");
-//		response.setCharacterEncoding("UTF-8");
-//		JObject jobject = new JObject();
-//		DBCon dbCon = new DBCon();
-//		try{
-//			String openId = request.getParameter("oid");
-//			String formId = request.getParameter("fid");
-//			String codeId = request.getParameter("codeId");
-//			String code = request.getParameter("code");
-//			
-//			dbCon.connect();
-//			dbCon.conn.setAutoCommit(false);
-//			
-//			// 验证验证码
-//			VerifySMSDao.verify(dbCon, new VerifyBuilder(Integer.valueOf(codeId), Integer.valueOf(code)));
-//			// 修改手机号码
-//			WeixinMemberDao.updateMobile(dbCon, request.getParameter("mobile"), openId, formId);
-//			
-//			dbCon.conn.commit();
-//			jobject.initTip(true, "操作成功, 已重新绑定手机号码.");
-//			
-//		}catch(BusinessException e){
-//			e.printStackTrace();
-//			jobject.initTip(e);
-//		}catch(SQLException e){
-//			e.printStackTrace();
-//			jobject.initTip(e);
-//		}finally{
-//			dbCon.disconnect();
-//			response.getWriter().print(jobject.toString());
-//		}
-		return null;
-	}
 	
-	/**
-	 * 微信前台绑定
-	 * @param mapping
-	 * @param form
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws Exception
-	 */
-/*	public ActionForward weixinFrontBind(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)	throws Exception {
-		request.setCharacterEncoding("UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		
-		String pin = (String) request.getAttribute("pin");
-		JObject jobject = new JObject();
-		try{
-			String weixinMemberCard = request.getParameter("weixinMemberCard");
-			String weixinMemberPhone = request.getParameter("weixinMemberPhone");
-			String weixinMemberName = request.getParameter("weixinMemberName");
-			String weixinMemberSex = request.getParameter("weixinMemberSex");
-			
-			Staff staff = StaffDao.verify(Integer.parseInt(pin));
-			
-			int memberId = WeixinMemberDao.bind(staff, weixinMemberPhone, Integer.parseInt(weixinMemberCard));
-			
-			Member.UpdateBuilder builder = new Member.UpdateBuilder(memberId);
-			
-			if(weixinMemberSex != null && !weixinMemberSex.isEmpty()){
-				builder.setSex(Member.Sex.valueOf(Integer.valueOf(weixinMemberSex)));
-			}
-			
-			if(weixinMemberName != null && !weixinMemberName.isEmpty()){
-				builder.setName(weixinMemberName);
-			}
-			
-			MemberDao.update(staff, builder);
-			
-			jobject.initTip(true, "操作成功, 已绑定会员信息.");
-		}catch(BusinessException e){
-			e.printStackTrace();
-			jobject.initTip(e);
-			
-		}catch(SQLException e){
-			e.printStackTrace();
-			jobject.initTip(e);
-		}finally{
-			response.getWriter().print(jobject.toString());
-		}
-		return null;
-	}	
-	*/
 }
