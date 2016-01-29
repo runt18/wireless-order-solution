5399d9ebc6e4e002d14bb4b331ca8fe314ef7832
yzhang1981 <21226674@qq.com>
2016-01-29 11:17:08 +0800

    add the weixin price to book & wx order

--- terminal/weixin/wx-term/weixin/order/js/order/order.js
+++ terminal/weixin/wx-term/weixin/order/js/order/order.js
@@ -21,20 +21,6 @@ $(function(){
 					'</div>' +
 					'<div {display}>菜名: <span style="font-weight: bold;">{foods}</span></div>' +
 				'</div>',
-		takeout: '<div class="box-order {orderBorder}">' +
-				'<div class="{orderClass}"><div style="height: 20px;">' +
-						'<div style="float: left;">订单号:{code}</div>' +
-						'<div style="float: right;color:#26a9d0;">{status}</div>' +
-				'</div></div>' +
-				'<div>点单日期: <span style="font-weight: bold">{orderDate}</span></div>' +
-				'<div>' +
-					'<div style="float: left">金额: <span style="font-weight: bold;color: red;">{totalPrice}</span> 元</div>' +
-					'<div style="float: right">菜品种类: <span style="font-weight: bold;color: red;">{count}</span> 项</div>' +
-				'</div>' +
-				'<div {display}>菜名: <span style="font-weight: bold;">{foods}</span></div>' +
-				'<div>电话: <span style="font-weight: bold;">{phone}</span></div>' +
-				'<div>地址: <span style="font-weight: bold;">{address}</span></div>' +			
-			'</div>',
 		book : '<div class="box-order orderBorder_commit">'	+
 					'<div class="box-order-commit"><div style="height: 20px;">' +
 						'<div style="float: left;">预订单号:{code}</div>' +
@@ -57,7 +43,7 @@ $(function(){
 	Util.lbar('', function(html){ $(document.body).append(html);  });
 	//获取订单列表
 	$.ajax({
-		url : '../../WXQueryOrder.do',
+		url : '../../WxQueryOrder.do',
 		dataType : 'json',
 		data : {
 			dataSource : 'getByMember',
@@ -67,8 +53,8 @@ $(function(){
 		},
 		success : function(data, status, xhr){
 			var html = [];
-			for(var i = 0; i < data.root.length; i++){
-				var temp = data.root[i];
+			//for(var i = 0; i < data.root.length; i++){
+			data.root.forEach(function(temp, i){
 				var count = temp.foods.length, foods = '';
 				for (var j = 0; j < temp.foods.length; j++) {
 					if(foods){
@@ -76,35 +62,19 @@ $(function(){
 					}
 					foods += temp.foods[j].foodName;
 				}
-				if(Util.mp.extra == 3){//外卖
-					html.push(Template.takeout.format({
-						code : temp.code,
-						status : temp.statusDesc,
-						orderDate : temp.date,
-						totalPrice : temp.price,
-						count : count,
-						foods : foods,
-						display : temp.statusVal == 1 ? 'hidden="hidden"' : '',
-						orderClass : temp.statusVal == 2?'box-order-commit' : '',
-						orderBorder : temp.statusVal == 2?'orderBorder_commit' : 'orderBorder_invalid',
-						phone : temp.contect.phone,
-						address : temp.contect.address
-					}));					
-				}else{//默认店内
-					html.push(Template.inside.format({
-						code : temp.code,
-						status : temp.statusDesc,
-						orderDate : temp.date,
-						totalPrice : temp.price,
-						count : count,
-						foods : foods,
-						display : temp.statusVal == 1 ? 'hidden="hidden"' : '',
-						orderClass : temp.statusVal == 2 ? 'box-order-commit' : '',
-						orderBorder : temp.statusVal == 2 ? 'orderBorder_commit' : 'orderBorder_invalid'
-					}));						
-				}
+				html.push(Template.inside.format({
+					code : temp.code,
+					status : temp.statusDesc,
+					orderDate : temp.date,
+					totalPrice : temp.price,
+					count : count,
+					foods : foods,
+					display : temp.statusVal == 1 ? 'hidden="hidden"' : '',
+					orderClass : temp.statusVal == 2 ? 'box-order-commit' : '',
+					orderBorder : temp.statusVal == 2 ? 'orderBorder_commit' : 'orderBorder_invalid'
+				}));						
+			});
 
-			}
 			$('#divWeixinOrderList').html(html.join(''));
 		},
 		error : function(xhr, errorType, error){}
@@ -135,13 +105,21 @@ $(function(){
 					}				
 				}
 
+				var totalPrice;
+				if(bookInfo.hasWxPay){
+					totalPrice = '已微信支付' + bookInfo.wxPayMoney;
+				}else if(bookInfo.price){
+					totalPrice = bookInfo.price;
+				}else{
+					totalPrice = 0;
+				}
 				
 				html.push(Template.book.format({
 					code : bookInfo.id,
 					status : bookInfo.statusDesc,
 					orderDate : bookInfo.bookDate,
 					orderRegion : bookInfo.region,
-					totalPrice : bookInfo.price ? bookInfo.price : 0,
+					totalPrice : totalPrice,
 					count : count,
 					display : !foods ? 'hidden="hidden"' : '',
 					foods : foods,
--- terminal/weixin/wx-term/weixin/order/js/wxAuth/authSuccess.js
+++ terminal/weixin/wx-term/weixin/order/js/wxAuth/authSuccess.js
@@ -1,46 +1,34 @@
-//从普通url获取当前桌信息
-function common_urlParaQuery() {
-	var name, value, i, key = 0;
-	var str = location.href;
+
 	
-	if(str.indexOf("#") > 0){
-		str = str.substring(0,str.indexOf("#"));
-	}
-	var num = str.indexOf("?");
-	if(num > 0){
-		str = str.substr(num + 1);
-		var arrtmp = str.split("&");
-		for (i = 0; i < arrtmp.length; i++) {
-			num = arrtmp[i].indexOf("=");
-			if (num > 0) {
-				name = arrtmp[i].substring(0, num);
-				value = arrtmp[i].substr(num + 1);
-				this[name] = value;
+$(function(){
+	
+	//从普通url获取当前桌信息
+	function common_urlParaQuery() {
+		var name, value, i, key = 0;
+		var str = location.href;
+		
+		if(str.indexOf("#") > 0){
+			str = str.substring(0,str.indexOf("#"));
+		}
+		var num = str.indexOf("?");
+		if(num > 0){
+			str = str.substr(num + 1);
+			var arrtmp = str.split("&");
+			for (i = 0; i < arrtmp.length; i++) {
+				num = arrtmp[i].indexOf("=");
+				if (num > 0) {
+					name = arrtmp[i].substring(0, num);
+					value = arrtmp[i].substr(num + 1);
+					this[name] = value;
+				}
 			}
 		}
 	}
-}
-
-function reAuth(){
-	$.ajax({
-		url : '../../WxAuthLogin.do',
-		type : 'post',
-		data : { rid : rid },
-		success : function(result){
-			console.log('FIXME : should be success');
-		},
-		error : function(result){
-			location.href = result.responseText;
-		}
-	});
-}
-
 
 	var Request = new common_urlParaQuery();
 	var rid = Request["rid"];
 	var authCode = Request["auth_code"];
 	
-$(function(){
 	$.ajax({
 		url : '../../WxAuth.do',
 		type : 'post',
@@ -59,15 +47,15 @@ $(function(){
 				},
 				dataType : 'json',
 				success : function(data){
-					$('#loading').hide();
+					$('#loading_div_authResponse').hide();
 					if(data.success){
 						var rest = data.root[0];
-						$('#wxRestLogo').attr('src', rest.headImgUrl);
-						$('#wxNickName').text(rest.nickName);
-						$('#authSuccess').show();
+						$('#wxRestLogo_img_authResponse').attr('src', rest.headImgUrl);
+						$('#wxNickName_label_authResponse').text(rest.nickName);
+						$('#authSuccess_div_authResponse').show();
 					}else{
-	//					$('#wxRestLogo').attr('src', 'images/member_card.jpg');
-	//					$('#wxNickName').text('品饺');
+	//					$('#wxRestLogo_img_authResponse').attr('src', 'images/member_card.jpg');
+	//					$('#wxNickName_label_authResponse').text('品饺');
 						$('#authFailure').show();			
 					}
 				},
@@ -78,8 +66,21 @@ $(function(){
 		}
 	});	
 	
+	//重新绑定
+	$('#reAuth_button_authResponse').click(function(){
+		$.ajax({
+			url : '../../WxAuthLogin.do',
+			type : 'post',
+			data : { rid : rid },
+			success : function(result){
+				console.log('FIXME : should be success');
+			},
+			error : function(result){
+				location.href = result.responseText;
+			}
+		});
+	});
 
-	
 });	
 	
 
