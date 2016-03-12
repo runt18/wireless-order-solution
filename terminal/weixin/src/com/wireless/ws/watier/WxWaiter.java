45aae881c1c70927c1048292c9513e64e73db0ae
yzhang1981 <21226674@qq.com>
2016-03-12 13:57:13 +0800

    add the wx waiter server point

---
+++ terminal/weixin/src/com/wireless/ws/watier/WxWaiter.java
@@ -0,0 +1,124 @@
+package com.wireless.ws.watier;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.websocket.Session;
+
+import com.wireless.json.JObject;
+import com.wireless.json.JsonMap;
+import com.wireless.json.Jsonable;
+import com.wireless.pojo.util.DateUtil;
+
+public class WxWaiter {
+
+	public static enum MsgType{
+		CALL_PAY(1, "呼叫结账");
+		
+		private final int val;
+		private final String desc;
+		
+		MsgType(int val, String desc){
+			this.val = val;
+			this.desc = desc;
+		}
+		
+		@Override
+		public String toString(){
+			return this.desc;
+		}
+	}
+	
+	public static class Msg implements Jsonable{
+		private final MsgType type;
+		private final String content;
+		
+		public Msg(MsgType type, String content){
+			this.type = type;
+			this.content = content;
+		}
+
+		@Override
+		public JsonMap toJsonMap(int flag) {
+			JsonMap jm = new JsonMap();
+			jm.putInt("type", type.val);
+			jm.putString("typeText", this.type.desc);
+			jm.putString("content", this.content);
+			jm.putString("hour", DateUtil.format(System.currentTimeMillis(), DateUtil.Pattern.HOUR));
+			return jm;
+		}
+
+		@Override
+		public void fromJsonMap(JsonMap jm, int flag) {
+		}
+		
+		@Override
+		public String toString(){
+			return "msg type = " + this.type.val + ", content = " + this.content;
+		}
+		
+	}
+	
+	public static class Msg4CallPay extends Msg{
+		public Msg4CallPay(String table, String expectPayType) {
+			super(MsgType.CALL_PAY, table + "客人呼叫" + (expectPayType != null ? "使用" + expectPayType : "") + "结账，请留意处理");
+		}
+		
+	}
+	
+    private final Set<Session> sessions = new CopyOnWriteArraySet<>();
+	private final int restaurantId;
+	
+	public WxWaiter(int restaurantId){
+		this.restaurantId = restaurantId;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if(obj == null || !(obj instanceof WxWaiter)){
+			return false;
+		}else{
+			return ((WxWaiter)obj).restaurantId == this.restaurantId;
+		}
+	}
+	
+	public void send(Msg msg){
+		for(Session session : sessions){
+			try{
+				JObject jObj = new JObject();
+				jObj.setRoot(msg);
+				session.getBasicRemote().sendText(jObj.toString());
+			}catch(IOException e){
+				try {
+					session.close();
+				} catch (IOException ignored) {
+					ignored.printStackTrace();
+				}
+				sessions.remove(session);
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	WxWaiter add(Session session){
+		this.sessions.add(session);
+		return this;
+	}
+	
+	WxWaiter remove(Session session){
+		this.sessions.remove(session);
+		return this;
+	}
+	
+	@Override
+	public int hashCode(){
+		return restaurantId * 31 + 17;
+	}
+	
+	@Override
+	public String toString(){
+		return "restaurantId = " + this.restaurantId + 
+			   ", session size = " + this.sessions.size();
+	}
+}
---
+++ terminal/weixin/src/com/wireless/ws/watier/WxWaiterServerPoint.java
@@ -0,0 +1,86 @@
+package com.wireless.ws.watier;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/ws/wx-waiter/{restaurantId}")
+public class WxWaiterServerPoint {
+ 
+    public static final List<WxWaiter> waiters = new CopyOnWriteArrayList<>();
+    
+    public WxWaiterServerPoint() {
+    	
+    }
+ 
+    public static WxWaiter getWxWaiter(int restaurantId){
+        int index = waiters.indexOf(new WxWaiter(restaurantId));
+        if(index >= 0){
+        	return waiters.get(index);
+        }else{
+        	return null;
+        }
+    }
+    
+    /**
+     * 打开连接
+     * 
+     * @param session
+     * @param restaurantId
+     */
+    @OnOpen
+    public void onOpen(Session session, @PathParam(value = "restaurantId") String restaurantId) {
+ 
+        WxWaiter waiter = getWxWaiter(Integer.parseInt(restaurantId));
+        if(waiter != null){
+        	waiter.add(session);
+        }else{
+        	waiters.add(new WxWaiter(Integer.parseInt(restaurantId)).add(session));
+        }
+        
+        System.out.println(restaurantId + ">" + "joined...");
+        
+    }
+ 
+    /**
+     * 关闭连接
+     */
+    @OnClose
+    public void onClose(Session session, CloseReason reason, @PathParam(value = "restaurantId") String restaurantId) {
+        WxWaiter waiter = getWxWaiter(Integer.parseInt(restaurantId));
+        if(waiter != null){
+        	waiter.remove(session);
+        }
+    	
+    	System.out.println(restaurantId + ">" + "closed..." + reason);
+    }
+ 
+    /**
+     * 接收信息
+     * 
+     * @param message
+     * @param restaurantId
+     */
+    @OnMessage
+    public void onMessage(String message, @PathParam(value = "restaurantId") String restaurantId) {
+        System.out.println((restaurantId + ">" + message));
+    }
+ 
+    /**
+     * 错误信息响应
+     * 
+     * @param throwable
+     */
+    @OnError
+    public void onError(@PathParam(value = "restaurantId") String restaurantId, Session session, Throwable throwable) {
+        System.out.println(restaurantId + ">" + throwable.getMessage());
+    }
+ 
+}
