6d103a4f234e059da659045be0727abab0368b3a
yzhang1981 <21226674@qq.com>
2016-03-15 16:35:37 +0800

    s

--- terminal/web/src/com/wireless/Actions/dishesOrder/InsertOrderAction.java
+++ terminal/web/src/com/wireless/Actions/dishesOrder/InsertOrderAction.java
@@ -9,6 +9,7 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.marker.weixin.api.BaseAPI;
 
 import com.wireless.db.staffMgr.StaffDao;
 import com.wireless.db.weixin.order.WxOrderDao;
@@ -19,6 +20,7 @@ import com.wireless.pack.ProtocolPackage;
 import com.wireless.pack.Type;
 import com.wireless.pack.req.ReqInsertOrder;
 import com.wireless.parcel.Parcel;
+import com.wireless.parcel.wrapper.IntParcel;
 import com.wireless.pojo.dishesOrder.Order;
 import com.wireless.pojo.dishesOrder.PrintOption;
 import com.wireless.pojo.staffMgr.Staff;
@@ -30,19 +32,23 @@ public class InsertOrderAction extends Action{
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		final JObject jobject = new JObject();
+		final String jsonText = request.getParameter("commitOrderData");
+		
+		final int type = Integer.parseInt(request.getParameter("type"));
+
+		final String notPrint = request.getParameter("notPrint");
+		
+		final String wxCode = request.getParameter("wxCode");
+		
+		final String orientedPrinter = request.getParameter("orientedPrinter");		
+		
+		final String wxWaiter = request.getParameter("wxWaiter");
+		
+		final JObject jObject = new JObject();
 		try {
 			final Staff staff = StaffDao.verify(Integer.parseInt((String)request.getAttribute("pin")));
 			
-			final String jsonText = request.getParameter("commitOrderData");
-			
-			final int type = Integer.parseInt(request.getParameter("type"));
 
-			final String notPrint = request.getParameter("notPrint");
-			
-			final String wxCode = request.getParameter("wxCode");
-			
-			final String orientedPrinter = request.getParameter("orientedPrinter");
 			
 			final PrintOption printOption;
 			if(notPrint != null && !notPrint.isEmpty()){
@@ -101,33 +107,55 @@ public class InsertOrderAction extends Action{
 					}
 				}
 				resp = ServerConnector.instance().ask(new ReqInsertOrder(staff, builder, printOption));
+				
 			}
 			
 			if(resp.header.type == Type.ACK){
-				jobject.initTip(true, ("下单成功."));
+				jObject.initTip(true, ("下单成功."));
+				//FIXME 新下单时打印【微信店小二】
+				//if(type == 1 && wxWaiter != null && !wxWaiter.isEmpty() && Boolean.parseBoolean(wxWaiter)){
+					final String serverName;
+					if(request.getServerName().equals("e-tones.net")){
+						serverName = "wx.e-tones.net";
+					}else{
+						serverName = request.getServerName(); 
+					}
+					final int serverPoint = request.getServerPort();
+					final int orderId = new Parcel(resp.body).readParcel(IntParcel.CREATOR).intValue();
+					new Thread(){
+						@Override
+						public void run(){
+							try {
+								BaseAPI.doGet("http://" + serverName + ":" + serverPoint + "/wx-term/WxOperateWaiter.do?dataSource=print&restaurantId=" + staff.getRestaurantId() + "&orderId=" + orderId);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}.start();
+				//}
 				
 			}else if(resp.header.type == Type.NAK){
 				ErrorCode errCode = new Parcel(resp.body).readParcel(ErrorCode.CREATOR);
-				jobject.initTip(false, errCode.getCode(), errCode.getDesc());
+				jObject.initTip(false, errCode.getCode(), errCode.getDesc());
 				
 			}else{
-				jobject.initTip(false, ("下单不成功，请重新确认."));
+				jObject.initTip(false, ("下单不成功，请重新确认."));
 			}
 			
 		}catch(BusinessException e){
-			jobject.initTip(false, JObject.TIP_TITLE_EXCEPTION, e.getCode(), e.getDesc());
+			jObject.initTip(false, JObject.TIP_TITLE_EXCEPTION, e.getCode(), e.getDesc());
 			e.printStackTrace();
 		}catch(IOException e){
-			jobject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9997, "服务器请求不成功，请重新检查网络是否连通.");
+			jObject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9997, "服务器请求不成功，请重新检查网络是否连通.");
 			e.printStackTrace();
 		}catch(NumberFormatException e){
-			jobject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9998, "菜品提交的数量不正确，请检查后重新提交.");
+			jObject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9998, "菜品提交的数量不正确，请检查后重新提交.");
 			e.printStackTrace();
 		}catch(Exception e){
-			jobject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9999, "操作失败, 数据库操作错误.");
+			jObject.initTip(false, JObject.TIP_TITLE_EXCEPTION, 9999, "操作失败, 数据库操作错误.");
 			e.printStackTrace();
 		}finally{
-			response.getWriter().print(jobject.toString());
+			response.getWriter().print(jObject.toString());
 		}
 		return null;
 	}
