72d5aa0493e5ee7cff87417d7c98c2660da51bd5
wire-zhijian <923679308@qq.com>
2016-05-13 18:31:20 +0800

    waiter.js

--- terminal/weixin/wx-term/weixin/order/js/weixinWaiter/waiter.js
+++ terminal/weixin/wx-term/weixin/order/js/weixinWaiter/waiter.js
@@ -550,7 +550,6 @@ $(function(){
 						} 
 						
 					}else{
-						checkPayTypeDialog.close();
 						payParam = null;
 						var dialog = new WeDialogPopup({
 							content : data.msg,
@@ -742,7 +741,6 @@ $(function(){
 			}, function(res) {
 				if (res.err_msg == "get_brand_wcpay_request:ok") {
 					// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-					checkPayTypeDialog.close();
 					orderFoodPopup.closeShopping();
 					$('#closeFastFood_a_waiter').click();
 					$('#foodList_div_waiter').html('');
