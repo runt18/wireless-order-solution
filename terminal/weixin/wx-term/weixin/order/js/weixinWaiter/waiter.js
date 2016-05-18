2077fb97e87bb436025d9dc8139ceddc95259008
wire-zhijian <923679308@qq.com>
2016-05-18 12:13:58 +0800

    waiter

--- terminal/weixin/wx-term/weixin/order/js/weixinWaiter/waiter.js
+++ terminal/weixin/wx-term/weixin/order/js/weixinWaiter/waiter.js
@@ -808,6 +808,7 @@ $(function(){
 				"signType" : payParam.signType,
 				"paySign" : payParam.paySign
 			}, function(res) {
+				alert(res.err_msg);
 				if (res.err_msg == "get_brand_wcpay_request:ok") {
 					// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
 					
@@ -835,7 +836,7 @@ $(function(){
 						}
 					});
 					
-					errDialog.open();
+					orderSuccessDialog.open();
 				} 
 			});
 		}
