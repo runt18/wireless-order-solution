3cb1356e8849f5334f09edafb92842c3953bd8cd
yzhang1981 <21226674@qq.com>
2016-07-14 12:33:46 +0800

    s

--- terminal/web/src/com/wireless/Actions/dailySettle/DailySettleExecAction.java
+++ terminal/web/src/com/wireless/Actions/dailySettle/DailySettleExecAction.java
@@ -1,8 +1,8 @@
 package com.wireless.Actions.dailySettle;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,66 +12,60 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
-import com.wireless.db.frontBusiness.DailySettleDao;
 import com.wireless.db.staffMgr.StaffDao;
 import com.wireless.exception.BusinessException;
+import com.wireless.exception.ErrorCode;
 import com.wireless.json.JObject;
-import com.wireless.json.JsonMap;
-import com.wireless.json.Jsonable;
-import com.wireless.pojo.billStatistics.DutyRange;
+import com.wireless.pack.ProtocolPackage;
+import com.wireless.pack.Type;
+import com.wireless.pack.req.ReqDailySettle;
+import com.wireless.parcel.Parcel;
+import com.wireless.pojo.printScheme.Printer;
 import com.wireless.pojo.staffMgr.Staff;
+import com.wireless.sccon.ServerConnector;
 
 public class DailySettleExecAction extends Action {
+	
+	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+		final String orientedPrinters = request.getParameter("orientedPrinter");
 
-		PrintWriter out = null;
-
-		JObject jObj = new JObject();		
+		final JObject jObject = new JObject();		
 		
 		try {
-			// 解决后台中文传到前台乱码
+
+			final String pin = (String)request.getAttribute("pin");
+
+			final Staff staff = StaffDao.verify(Integer.parseInt(pin));
+
+			final List<Printer> printers = new ArrayList<>();
 			
-			out = response.getWriter();
-
-			String pin = (String)request.getAttribute("pin");
-
-			Staff staff = StaffDao.verify(Integer.parseInt(pin));
-
-			final DutyRange dutyRange = DailySettleDao.manual(staff).getRange();
+			if(orientedPrinters != null && !orientedPrinters.isEmpty()){
+				for(String printerId : orientedPrinters.split(",")){
+					printers.add(new Printer(Integer.parseInt(printerId)));
+				}
+			}
 			
-			jObj.setExtra(new Jsonable(){
-
-				@Override
-				public JsonMap toJsonMap(int flag) {
-					JsonMap jm = new JsonMap();
-					jm.putJsonable("dutyRange", dutyRange, 0);
-					return jm;
-				}
-
-				@Override
-				public void fromJsonMap(JsonMap jsonMap, int flag) {
-					
-				}
-				
-			});
-
-			jObj.initTip(true, staff.getName() + "日结成功");
+			ProtocolPackage resp = ServerConnector.instance().ask(new ReqDailySettle(staff, printers));
+			
+			if(resp.header.type == Type.ACK){
+				jObject.initTip(true, staff.getName() + "日结成功");
+			}else{
+				jObject.initTip(false, new Parcel(resp.body).readParcel(ErrorCode.CREATOR).getDesc());
+			}
+			
 
 		} catch (BusinessException e) {
 			e.printStackTrace();
-			jObj.initTip(false, e.getDesc());
+			jObject.initTip(false, e.getDesc());
 			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			jObj.initTip(false, "数据库请求发生错误，请确认网络是否连接正常");
-
 		} catch (IOException e) {
 			e.printStackTrace();
-			jObj.initTip(false, "数据库请求发生错误，请确认网络是否连接正常");
+			jObject.initTip(false, "数据库请求发生错误，请确认网络是否连接正常");
 
 		} finally {
-			out.write(jObj.toString());
+			response.getWriter().write(jObject.toString());
 		}
 
 		return null;
