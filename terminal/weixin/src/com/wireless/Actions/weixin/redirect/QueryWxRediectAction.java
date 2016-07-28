9be20627bd6854c44b8de6b6588616cefd0f72a5
wire-zhijian <923679308@qq.com>
2016-07-28 10:40:24 +0800

    wx

--- terminal/weixin/src/com/wireless/Actions/weixin/redirect/QueryWxRediectAction.java
+++ terminal/weixin/src/com/wireless/Actions/weixin/redirect/QueryWxRediectAction.java
@@ -43,13 +43,17 @@ public class QueryWxRediectAction extends DispatchAction {
 			final String path2 = "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
 			final String encoderUrl = "http://" + serverName + "/wx-term/weixin/order/redirect.html?fid=" + wxRestaurant.getWeixinSerial();
 			if(WxHandleMessage.EventKey.SELF_ORDER_EVENT_KEY.getKey().equals(key)){
-				url = path1 + URLEncoder.encode(encoderUrl + "&href=wxOrder.html", "utf-8") + path2;
+				url = path1 + URLEncoder.encode(encoderUrl + "&href=food.html", "utf-8") + path2;
 			}else if(WxHandleMessage.EventKey.SELF_BOOK_EVENT_KEY.getKey().equals(key)){
 				url = path1 + URLEncoder.encode(encoderUrl + "&href=book.html", "utf-8") + path2;
 			}else if(WxHandleMessage.EventKey.MEMBER_EVENT_KEY.getKey().equals(key)){
 				url = path1 + URLEncoder.encode(encoderUrl + "&href=member.html", "utf-8") + path2;
 			}else if(WxHandleMessage.EventKey.ORDER_EVENT_KEY.getKey().equals(key)){
 				url = path1 + URLEncoder.encode(encoderUrl + "&href=orderList.html", "utf-8") + path2;
+			}else if(WxHandleMessage.EventKey.PROMOTION_EVENT_KEY.getKey().equals(key)){
+				url = path1 + URLEncoder.encode(encoderUrl, "&href=sales.html") + path2;
+			}else if(WxHandleMessage.EventKey.I_WANT_REPRESENT.getKey().equals(key)){
+				url = path1 + URLEncoder.encode(encoderUrl, "&href=representCard.html") + path2;
 			}else{
 				throw new BusinessException("该功能不支持直接跳转..");
 			}
