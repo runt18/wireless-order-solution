3e3eb12828be8494305fdfc1a1d54839f9775e2c
wire-zhijian <923679308@qq.com>
2016-07-29 14:48:49 +0800

    orderList

---
+++ terminal/weixin/wx-term/weixin/order/js/order/orderList.js
@@ -0,0 +1,26 @@
+$(function(){
+//	var template = {
+//		order : 
+//	
+//	};
+	
+	new CreateTabPanel([{
+		tab : '订单列表',
+		initTab : 0,
+		onActive : function(){
+			console.log(0 + '------active');
+		},
+		onDeactive : function(){
+			console.log(0 + '------Deactive');
+		}
+	}, {
+		tab : '预订列表',
+		initTab : 1,
+		onActive : function(){
+			console.log(1 + '------active');	
+		},
+		onDeactive : function(){
+			console.log(1 + '------Deactive');
+		}
+	}]).open();
+});
