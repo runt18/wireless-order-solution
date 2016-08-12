488f9563301e7d4385379f737d9122868508064e
lulincan <1043508768@qq.com>
2016-08-12 15:13:46 +0800

    d

---
+++ db/scripts/update_db_v136_expiredDuration.sql
@@ -0,0 +1,22 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_SAFE_UPDATES=@@SQL_SAFE_UPDATES;
+SET SQL_SAFE_UPDATES = 0;
+SET NAMES utf8;
+USE wireless_order_db;
+
+-- -----------------------------------------------------
+-- Table `wireless_order_db`.`couponType`
+-- -----------------------------------------------------
+ALTER TABLE `wireless_order_db`.`coupon_type` 
+ADD COLUMN `expired_duration` INT NULL DEFAULT NULL AFTER `limit_amount`,
+ADD COLUMN `expired_type` INT NULL DEFAULT 1 AFTER `expired_duration`;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET SQL_SAFE_UPDATES = @OLD_SAFE_UPDATES;
+
+
+
